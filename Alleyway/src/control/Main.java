23ec61cb2dcc07b540105a638f3103c26ed49edc
crackdog <flo.kahllund@googlemail.com>
2013-08-08 01:54:18 +0200

    adding hello world

--- Alleyway/src/control/Main.java
+++ Alleyway/src/control/Main.java
@@ -19,6 +19,8 @@ public class Main {
      */
     public static void main(String args[]) {
         
+        System.out.println("Hello World!");
+        
         Model game = new Model();        
         View frame = new View();
         Controller ctrl = new Controller(game, frame);
